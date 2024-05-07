c15173e28cdd5f7b1a299fc2efd875ed58dbbb4e
Tim Gremore <tim@headway.io>
2024-05-07 11:08:53 -0500

    chore: Version bump

--- checksum-Elixir.Edix.Parser.exs
+++ checksum-Elixir.Edix.Parser.exs
@@ -1,3 +1,3 @@
 %{
-  "libedix_parser-v0.1.1-nif-2.15-aarch64-apple-darwin.so.tar.gz" => "sha256:ed19c1f019613fe0b3166047c449cbddae4e6c01c5b953993dcdd13d6151b01b",
+  "libedix_parser-v0.1.2-nif-2.15-aarch64-apple-darwin.so.tar.gz" => "sha256:69ad660fd29711a774b72d7e7721ee2b03c53ac80a5ef3a48c22a97b89e21ea1",
 }
