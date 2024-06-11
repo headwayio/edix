4f3d73c7badd077fb8f72d8f7f612b94eccaebee
Tim Gremore <tim@headway.io>
2024-06-11 12:15:44 -0500

    chore: Update checksum

--- checksum-Elixir.Edix.Parser.exs
+++ checksum-Elixir.Edix.Parser.exs
@@ -1,3 +1,12 @@
 %{
-  "libedix_parser-v0.1.9-nif-2.15-aarch64-apple-darwin.so.tar.gz" => "sha256:a77db573b79f79f53023a6e7594b0e10f1eda2780be6c5d16ac318993fd3faae",
+  "edix_parser-v0.2.3-nif-2.15-x86_64-pc-windows-gnu.dll.tar.gz" => "sha256:a53dab053e5d86dd99f2e7cb69d99330d94b0bbafd6943b914b84275e8d8c0bd",
+  "edix_parser-v0.2.3-nif-2.15-x86_64-pc-windows-msvc.dll.tar.gz" => "sha256:a64d4f33d2e9a813438d5401390ce971242297607a4a21fea1733a24e8a16b5a",
+  "libedix_parser-v0.2.3-nif-2.15-aarch64-apple-darwin.so.tar.gz" => "sha256:e85ae78f50f2eeb7e22b8c3bd65c90f43e5301f313bb3982b3b38ebb9261883f",
+  "libedix_parser-v0.2.3-nif-2.15-aarch64-unknown-linux-gnu.so.tar.gz" => "sha256:dec92405405b08aa1d60727efa838f0d1d84f2b9c14f40dba2fd14107e563b3f",
+  "libedix_parser-v0.2.3-nif-2.15-aarch64-unknown-linux-musl.so.tar.gz" => "sha256:ae26924ddbedf24cc3916fd69cd7a8c25762a1bf2d07b03e9ed41c0542fdd1c0",
+  "libedix_parser-v0.2.3-nif-2.15-arm-unknown-linux-gnueabihf.so.tar.gz" => "sha256:eae2355dc43eff1f8bc2dfd7e060d9b849f106fbcb75750b414b99575d0a92ff",
+  "libedix_parser-v0.2.3-nif-2.15-riscv64gc-unknown-linux-gnu.so.tar.gz" => "sha256:1bed82e5d9d214b6ca760dd4a59404b2054d3e154f0ad8b52e235ea1967146c0",
+  "libedix_parser-v0.2.3-nif-2.15-x86_64-apple-darwin.so.tar.gz" => "sha256:b866b6b62e7db344e83bf62fbb14bf04c4a72f7c48dbf41a1f1c112bdc2b4cf4",
+  "libedix_parser-v0.2.3-nif-2.15-x86_64-unknown-linux-gnu.so.tar.gz" => "sha256:4fa093dc80a4ec3e008087046dec547888f32ebc66f011bec65c4c51edbee885",
+  "libedix_parser-v0.2.3-nif-2.15-x86_64-unknown-linux-musl.so.tar.gz" => "sha256:3ef04dada824d648a054feb956ca8c80e8f2da86ab1537d6ca79cb2ba9cffd52",
 }
