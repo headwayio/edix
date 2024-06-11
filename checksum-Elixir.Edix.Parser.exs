07103b67440068d0d2977a9f294552028bfaa243
Tim Gremore <tim@headway.io>
2024-06-11 10:38:29 -0500

    wip: Attempt compiling all targets

--- checksum-Elixir.Edix.Parser.exs
+++ checksum-Elixir.Edix.Parser.exs
@@ -1,3 +1,3 @@
 %{
-  "libedix_parser-v0.1.7-nif-2.15-aarch64-apple-darwin.so.tar.gz" => "sha256:c2f98407789ee0ce85b79afa22d25b7469e0a3c08732a0ce7083f83e2adbc052",
+  "libedix_parser-v0.1.9-nif-2.15-aarch64-apple-darwin.so.tar.gz" => "sha256:a77db573b79f79f53023a6e7594b0e10f1eda2780be6c5d16ac318993fd3faae",
 }
